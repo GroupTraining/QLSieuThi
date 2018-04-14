e77ebacfecdaf8e5718e95d87960febabef193f8
giangttvm <36163538+giangttvm@users.noreply.github.com>
2018-04-14 14:50:33 +0700

    code form Editkh

--- QL_SieuThi/BUS/Bus.cs
+++ QL_SieuThi/BUS/Bus.cs
@@ -24,5 +24,53 @@ namespace BUS
                 return 0;
             }
         }
+
+        //chỉnh sửa khách hàng 
+         public object get_KhachHang()
+        {
+            var kh = from p in data.Khach_Hangs
+                     select new
+                     {
+                         MaKH = p.MaKH,
+                         TenKH = p.TenKH,
+                         DiaChi = p.DiaChi,
+                         SDT = p.SDT
+
+                     };
+            return kh;
+            
+        }
+
+        public object AddKhachHang(string makh,string tenkh,string diachi,string sdt)
+        {
+            Khach_Hang a = new Khach_Hang();
+            a.MaKH = makh;
+            a.TenKH = tenkh;
+            a.DiaChi = diachi;
+            a.SDT = sdt;
+            return 1;
+        }
+
+        public object EditKhachHang(string makh,string tenkh,string diachi,string sdt)
+        {
+            var p= data.Khach_Hangs.Single(a => a.MaKH == makh);
+            p.TenKH = tenkh;
+            p.DiaChi = diachi;
+            p.SDT = sdt;
+            data.SubmitChanges();
+            return 1;
+
+        }
+
+        public object DelKhachHang(string makh,string tenkh,string diachi,string  sdt)
+        {
+            var kh = data.Khach_Hangs.Single(a => a.MaKH == makh);
+            var hdbh = data.HoaDon_BanHangs.Single(a => a.MaKH == makh);
+            data.Khach_Hangs.DeleteOnSubmit(kh);
+            data.HoaDon_BanHangs.DeleteOnSubmit(hdbh);
+            data.SubmitChanges();
+            return 1; ;
+
+        }
     }
 }
